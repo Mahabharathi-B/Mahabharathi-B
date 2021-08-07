60033f2de63b0b453783ee5009432705f73fcc6c
Mahabharathi-B <85634923+Mahabharathi-B@users.noreply.github.com>
2021-08-07 11:47:32 +0530

    Add files via upload

---
+++ CupCakes.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class CupCakes{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int testCases=sc.nextInt();
+int packed,leftOver;
+int arr[]=new int[testCases];
+for(int i=0;i<testCases;i++){
+   arr[i]=sc.nextInt();
+}
+for(int i=0;i<testCases;i++){
+if(arr[i]==2||arr[i]==1)
+ System.out.println(arr[i]);
+else{
+   leftOver=(arr[i]/2)+1;
+   packed=arr[i]-leftOver;
+System.out.println(leftOver);
+}
+}
+}
+}
